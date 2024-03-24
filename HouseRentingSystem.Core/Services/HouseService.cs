c6457ee9847ed1783526836ea39d661828e55c83
RayaDV <raya.d.dimitrova@gmail.com>
2024-03-25 00:27:18 +0200

    House Service added.

---
+++ HouseRentingSystem.Core/Services/HouseService.cs
@@ -0,0 +1,32 @@
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Infrastructure.Common;
+using HouseRentingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class HouseService : IHouseService
+    {
+        private readonly IRepository repository;
+
+        public HouseService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+        {
+            return await repository
+                .AllReadOnly<Infrastructure.Data.Models.House>()
+                .OrderByDescending(h => h.Id)
+                .Select(h => new HouseIndexServiceModel
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    ImageUrl = h.ImageUrl
+                })
+                .Take(3) // to here everything is "expression tree" and finaly we matherialize it; Take(3) may stay before proection.
+                .ToListAsync();
+        }
+    }
+}

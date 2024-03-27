0cd6933ebde309f3963f7f28b171aa5a5cdc6a04
RayaDV <raya.d.dimitrova@gmail.com>
2024-03-27 23:54:38 +0200

    DecimalModelBinder added.

---
+++ HouseRentingSystem/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HouseRentingSystem.ModelBindders
+{
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(decimal) ||
+                context.Metadata.ModelType == typeof(decimal?))
+            {
+                return new DecimalModelBinder();
+            }
+
+            return null;  // if return null, it will be ignored and will go to the next modelBinder
+        }
+    }
+}
--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,4 +1,5 @@
 using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.ModelBindders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+});
 
 builder.Services.AddApplicationServices();
 
